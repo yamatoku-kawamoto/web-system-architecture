f4aac8ae48573b01b578da1b5b3b7eaf30806613
yamatoku-kawamoto <160816989+yamatoku-kawamoto@users.noreply.github.com>
2024-12-24 16:21:17 +0900

    update:  configを更新

--- views/vite.config.ts
+++ views/vite.config.ts
@@ -3,44 +3,42 @@ import solid from "vite-plugin-solid";
 
 import inputPlugin from "@macropygia/vite-plugin-glob-input";
 import suidPlugin from "@suid/vite-plugin";
-
+import { resolve } from "node:path";
 // @ts-expect-error
 import handlebars from "vite-plugin-handlebars";
-import { resolve } from "node:path";
 
 export default defineConfig({
-  root: "./",
-  publicDir: "public",
-  assetsInclude: ["public/**/*"],
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
-  plugins: [
-    solid(),
-    suidPlugin(),
-    inputPlugin({
-      patterns: ["./src/templates/**/*.html","./src/partials/**/*.html"],
-    }),
-    handlebars({
-      partialDirectory: resolve(__dirname, "./src/components"),
-    }),
-  ],
-  css: {
-    modules: {
-      localsConvention: "dashes",
-    },
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@src": resolve(__dirname, "./src"),
-      "@components": resolve(__dirname, "./src/components"),
-      "@assets": resolve(__dirname, "./src/assets"),
-    },
-  },
+	root: "./",
+	publicDir: "public",
+	assetsInclude: ["public/**/*"],
+	build: {
+		outDir: "dist",
+		sourcemap: true,
+	},
+	plugins: [
+		solid(),
+		suidPlugin(),
+		inputPlugin({
+			patterns: ["./src/templates/**/*.html", "./src/components/**/*.html"],
+		}),
+		handlebars({
+			partialDirectory: resolve(__dirname, "./src/partials"),
+		}),
+	],
+	css: {
+		modules: {
+			localsConvention: "dashes",
+		},
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler",
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@src": resolve(__dirname, "./src"),
+			"@partial": resolve(__dirname, "./src/partials"),
+		},
+	},
 });
